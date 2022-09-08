2b420c023f754dc04c4ae67f9c26869c48731aca
Yolima Alejandra Guadir <105995641+alejandraguadir@users.noreply.github.com>
2022-09-08 15:42:08 -0500

    Delete 1. CANTERAS/4.SEMANA-TALLERES CON JAVA directory
    No corresponde al taller de variables

--- 1. CANTERAS/4.SEMANA-TALLERES CON JAVA/2. TALLER CONDICIONALES CON JAVA/2.5drogueria/drogueria/src/App.java
+++
@@ -1,8 +0,0 @@
-import View.drogueriaView;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        drogueriaView menudView = new drogueriaView();
-        menudView.menu();
-    }
-}
--- 1. CANTERAS/4.SEMANA-TALLERES CON JAVA/2. TALLER CONDICIONALES CON JAVA/2.7indicemc/indicemc/src/App.java
+++
@@ -1,9 +0,0 @@
-import aplicacion.indicemc;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        indicemc indicemasacorporal = new indicemc();
-        indicemasacorporal.menu();
-
-    }
-}
