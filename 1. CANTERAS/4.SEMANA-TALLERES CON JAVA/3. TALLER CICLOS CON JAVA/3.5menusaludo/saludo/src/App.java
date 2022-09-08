987bda975037033b229585be63d9e7f43ffea661
Yolima Alejandra Guadir <105995641+alejandraguadir@users.noreply.github.com>
2022-09-08 09:54:41 -0500

    Delete 1. CANTERAS/4.SEMANA-TALLERES CON JAVA directory
    No corresponden al taller de arreglos

--- 1. CANTERAS/4.SEMANA-TALLERES CON JAVA/3. TALLER CICLOS CON JAVA/3.5menusaludo/saludo/src/App.java
+++
@@ -1,8 +0,0 @@
-import View.PersonaView;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        PersonaView pView = new PersonaView();
-        pView.menu();
-    }
-}
