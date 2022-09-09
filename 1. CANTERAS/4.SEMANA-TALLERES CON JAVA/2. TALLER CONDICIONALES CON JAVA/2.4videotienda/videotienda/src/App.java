f9155b6149756f60e15ee0f1f4affe28e16ba3df
Yolima Alejandra Guadir <105995641+alejandraguadir@users.noreply.github.com>
2022-09-08 20:26:38 -0500

    Delete 1. CANTERAS/4.SEMANA-TALLERES CON JAVA directory

--- 1. CANTERAS/4.SEMANA-TALLERES CON JAVA/2. TALLER CONDICIONALES CON JAVA/2.4videotienda/videotienda/src/App.java
+++
@@ -1,9 +0,0 @@
-import View.menuVideotieda;
-
-public class App {
-    public static void main(String[] args)  {
-        menuVideotieda menuView = new menuVideotieda();
-        menuView.menu();
-        
-    }
-}
--- 1. CANTERAS/4.SEMANA-TALLERES CON JAVA/3. TALLER CICLOS CON JAVA/3.6agenda/agenda/src/App.java
+++
@@ -1,8 +0,0 @@
-import View.ContactoView;
-
-public class App {
-    public static void main(String[] args){
-        ContactoView cView = new ContactoView();
-        cView.menu();
-    }
-}
